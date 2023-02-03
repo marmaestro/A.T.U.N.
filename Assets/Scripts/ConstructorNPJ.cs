506b6f849667f4bcdfa7cff3ba115755d5d3cbbd
AlbertoAlanSA <83225413+AlbertoAlanSA@users.noreply.github.com>
2023-02-04 00:50:08 +0100

    npj prefab

---
+++ Assets/Scripts/ConstructorNPJ.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructorNPJ : MonoBehaviour
+{
+    public string Name;
+    public string Subtitle;
+    public string NextPj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
