68b1c48f6d5ef4e5a90d2bb6704774d88094e637
AlbertoAlanSA <83225413+AlbertoAlanSA@users.noreply.github.com>
2023-02-04 02:12:51 +0100

    cuadro de texto

--- Assets/Scripts/EnseñarTexto.cs
+++ Assets/Scripts/EnseñarTexto.cs
@@ -6,20 +6,24 @@ using TMPro;
 public class EnseñarTexto : MonoBehaviour
 {
     public GameObject CuadroTexto;
+    public GameObject PJ;
+
     private GameObject textoNombre;
     private GameObject textoSubtitulo;
+    private float distanciaAPJ;
     
     // Start is called before the first frame update
     void Start()
     {
+        distanciaAPJ=2.5f;
         //this.gameObject.GetComponent<ConstructoNPJs>.Name;
-        MostrarTexto();
+        //MostrarTexto();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(DistanciaAPjAdecuada()) MostrarTexto();
     }
 //dialogSpace = GameObject.Find("Canvas").transform.GetChild(3).gameObject;
     void MostrarTexto(){
@@ -31,5 +35,9 @@ public class EnseñarTexto : MonoBehaviour
         //asignarlos a cosas
         textoNombre.GetComponent<TextMeshProUGUI>().text=this.gameObject.GetComponent<ConstructorNPJ>().NpjName;
         textoSubtitulo.GetComponent<TextMeshProUGUI>().text=this.gameObject.GetComponent<ConstructorNPJ>().Subtitle;
+    }
+    bool DistanciaAPjAdecuada(){
+        print(PJ.GetComponent<Transform>().position +", "+ transform.position+", "+distanciaAPJ+": "+(Vector3.Distance(PJ.GetComponent<Transform>().position, transform.position)<distanciaAPJ));
+        return(Vector3.Distance(PJ.GetComponent<Transform>().position, transform.position)<distanciaAPJ);
     }
 }
