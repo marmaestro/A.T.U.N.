c73859b32b7dfece4b371ab0d1bda482fe54b2a2
Yui <94931996+xXYuiKXx@users.noreply.github.com>
2023-02-04 02:37:50 +0100

    Canvas prefab

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public float fadeDuration = 1.0f;
+    public float displayImageDuration = 1.0f;
+    //public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    float m_Timer;
+
+    //Configuración del texto de la escena
+    public Text TittleText;
+    public Text DescriptionText;
+    
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    void EndLevel()
     {
-        
+        m_Timer = Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
     }
+
+    
 }
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string SceneName;
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
